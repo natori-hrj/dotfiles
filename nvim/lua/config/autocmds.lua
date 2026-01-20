ef4cb242957e7699e23d4c57cdbd0a092b296f62
natori-hrj <al4ud1de@gmail.com>
2026-01-20 14:08:25 +0900

    add:starship

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -13,3 +13,14 @@ vim.api.nvim_create_autocmd("User", {
     vim.cmd("Neotree show")
   end,
 })
+
+-- Python用のオートコマンド
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "python",
+  callback = function()
+    vim.opt_local.tabstop = 4
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.expandtab = true
+    vim.opt_local.autoindent = true
+  end,
+})
--- nvim/lua/plugins/python.lua
+++ nvim/lua/plugins/python.lua
@@ -1,78 +1,113 @@
--- python
---
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-vim.opt.autoindent = true
+return {
+  -- Mason (LSPインストーラー)
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "pyright" },
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = { "williamboman/mason-lspconfig.nvim" },
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.pyright.setup({
+        settings = {
+          python = {
+            analysis = {
+              typeCheckingMode = "basic",
+              autoSearchPaths = true,
+              useLibraryCodeForTypes = true,
+              diagnosticMode = "workspace",
+            },
+          },
+        },
+      })
+    end
+  },
 
--- LSP
---
-require("mason").setup()
-require("meson-lspconfig").setup({
-  ensure_installed = { "pyright" },
-})
+  -- 補完プラグイン
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+    },
+    config = function()
+      local cmp = require("cmp")
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+        }, {
+          { name = "buffer" },
+        }),
+      })
+    end
+  },
 
-local lspconfig = require("lspconfig")
-lspconfig.pyright.setup({
-  settings = {
-    python = {
-      analysis = {
-        typeCheckingMode = "basic",
-        autoSearchPaths = true,
-        useLibraryCodeForTypes = true,
-        diagnosticMode = "workspace",
-      },
+  -- Treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    main = "nvim-treesitter.configs",
+    opts = {
+      ensure_installed = { "python", "lua" },
+      highlight = { enable = true },
+      indent = { enable = true },
     },
   },
-})
 
--- 補完
---
-local cmp = require("cmp")
-cmp.setup({
-  snippet = {
-    expand = function(args)
-      require("luaship").lsp_expand(args.body)
-    end,
+  -- フォーマッター
+  {
+    "stevearc/conform.nvim",
+    config = function()
+      require("conform").setup({
+        formatters_by_ft = {
+          python = { "black", "isort" },
+        },
+        format_on_save = {
+          timeout_ms = 500,
+          lsp_fallback = true,
+        },
+      })
+    end
   },
-  mappping = cmp.mappping.preset.insert({
-    ["<C-b>"] = cmp.mappping.scrool_docs(-4),
-    ["<C-f>"] = cmp.mappping.scrool_docs(4),
-    ["<C-Space>"] = cmp.mappping.complete(),
-    ["<CR>"] = cmp.mappping.confirm({ select = true }),
-  }),
-  sources = cmp.config.sources({
-    { name = "nvim_lsp" },
-    { name = "luaship" },
-  }, {
-    { name = "buffer" },
-  }),
-})
 
--- Treesitter設定
-require("nvim-treesitter.configs").setup({
-  ensure_installed = { "python" },
-  highlight = { enable = true },
-  indent = { enable = true },
-})
-
--- フォーマッター設定（conform.nvim）
-require("conform").setup({
-  formatters_by_ft = {
-    python = { "black", "isort" },
+  -- Linter
+  {
+    "mfussenegger/nvim-lint",
+    config = function()
+      require("lint").linters_by_ft = {
+        python = { "ruff" },
+      }
+      vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+        callback = function()
+          require("lint").try_lint()
+        end,
+      })
+    end
   },
-  format_on_save = {
-    timeout_ms = 500,
-    lsp_fallback = true,
-  },
-})
-
--- Linter設定（nvim-lint）
-require("lint").linters_by_ft = {
-  python = { "ruff" }, -- または "flake8", "pylint"
 }
-vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-  callback = function()
-    require("lint").try_lint()
-  end,
-})
